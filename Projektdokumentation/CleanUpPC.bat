5f0da2c4ce171712584982046ad35527eed73879
Mick Kirkegaard <mickkn@gmail.com>
2015-02-25 22:33:41 +0100

    Underskriftside + bat fil opdateret
    Nu kigger CleanUpPC.bat ned i all undermapper og sletter junk filer :)

---
+++ Projektdokumentation/CleanUpPC.bat
@@ -0,0 +1,11 @@
+echo off
+FOR /r %%f IN (Master.pdf) do del %%f
+FOR /r %%f IN (*.aux) do del %%f
+FOR /r %%f IN (*.log) do del %%f
+FOR /r %%f IN (*.out) do del %%f
+FOR /r %%f IN (*.DS_Store) do del %%f
+FOR /r %%f IN (*.gz) do del %%f
+FOR /r %%f IN (*.toc) do del %%f
+FOR /r %%r IN (Thumbs.db) do del %%f
+
+echo on
